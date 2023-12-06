2b7779273a348918d48b0cd4dd9342c965de0873
Mirty <mirtybmx@gmail.com>
2023-12-06 16:12:26 -0300

    Faltan mejoras en el front

---
+++ src/main/java/com/backend/clinicaodontologica/controller/OdontologoController.java
@@ -0,0 +1,56 @@
+package com.backend.clinicaodontologica.controller;
+
+import com.backend.clinicaodontologica.dto.entrada.odontologo.OdontologoEntradaDto;
+import com.backend.clinicaodontologica.dto.modificacion.OdontologoModificacionEntradaDto;
+import com.backend.clinicaodontologica.dto.salida.odontologo.OdontologoSalidaDto;
+
+import com.backend.clinicaodontologica.exceptions.ResourceNotFoundException;
+import com.backend.clinicaodontologica.service.impl.IOdontologoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/odontologos")
+public class OdontologoController {
+    public IOdontologoService odontologoService;
+
+    public OdontologoController(IOdontologoService odontologoService) {
+        this.odontologoService = odontologoService;
+    }
+    @GetMapping("/buscarIdDeOdontologo")
+    public String buscarOdontologoPorId(Model model, @RequestParam Long id){
+        OdontologoSalidaDto odontologo = odontologoService.buscarOdontologoPorId(id);
+
+        model.addAttribute("nombre", odontologo.getNombre());
+        model.addAttribute("apellido", odontologo.getApellido());
+
+        return "odontologo";
+    }
+    @GetMapping("/buscarId/{id}")
+    public OdontologoSalidaDto buscarPorId(@PathVariable Long id){
+        return  odontologoService.buscarOdontologoPorId(id);
+
+
+    }
+
+    @GetMapping("/listar")
+    public ResponseEntity<List<OdontologoSalidaDto>> listarOdontologos(){
+        return new ResponseEntity<>(odontologoService.listarOdontologo(), HttpStatus.OK);
+    }
+    @PostMapping("/registrar")
+    public ResponseEntity<OdontologoSalidaDto> guardar(@RequestBody OdontologoEntradaDto odontologo){
+        return new ResponseEntity<>(odontologoService.registrarOdontologo(odontologo),HttpStatus.OK);
+    }
+    @DeleteMapping("/eliminar/{id}")
+    public void eliminarOdontologo(@PathVariable Long id){
+        odontologoService.eliminarOdontologo(id);
+    }
+    @PutMapping("/actualizar")
+    public ResponseEntity<OdontologoSalidaDto> actualizarOdontologo(@RequestBody OdontologoModificacionEntradaDto odontologo) throws ResourceNotFoundException {
+        return new ResponseEntity<>(odontologoService.actualizarOdontologo(odontologo),HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/backend/clinicaodontologica/controller/PacienteController.java
@@ -0,0 +1,67 @@
+package com.backend.clinicaodontologica.controller;
+
+import com.backend.clinicaodontologica.dto.entrada.paciente.PacienteEntradaDto;
+import com.backend.clinicaodontologica.dto.modificacion.PacienteModificacionEntradaDto;
+import com.backend.clinicaodontologica.dto.salida.paciente.PacienteSalidaDto;
+
+import com.backend.clinicaodontologica.exceptions.ResourceNotFoundException;
+import com.backend.clinicaodontologica.service.impl.IPacienteService;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/pacientes")
+public class PacienteController {
+
+    private IPacienteService pacienteService;
+
+    public PacienteController(IPacienteService pacienteService) {
+        this.pacienteService = pacienteService;
+    }
+
+
+    @GetMapping("/buscarId")
+    public String buscarPacientePorId(Model model, @RequestParam Long id){
+        PacienteSalidaDto paciente = pacienteService.buscarPacientePorId(id);
+
+        model.addAttribute("nombre", paciente.getNombre());
+        model.addAttribute("apellido", paciente.getApellido());
+
+        return "paciente";
+
+    }
+
+
+    @GetMapping("/listar")
+    public ResponseEntity<List<PacienteSalidaDto>> listarPacientes(){
+        return new ResponseEntity<>(pacienteService.listarPacientes(), HttpStatus.OK);
+    }
+    @PostMapping("/registrar")
+    public ResponseEntity<PacienteSalidaDto> guardar(@RequestBody @Valid PacienteEntradaDto paciente){
+       return new ResponseEntity<>(pacienteService.registrarPaciente(paciente), HttpStatus.CREATED);
+    }
+    @DeleteMapping("/eliminar/{id}")
+    public  ResponseEntity<Void> eliminarPaciente(@PathVariable Long id)throws ResourceNotFoundException {
+        pacienteService.eliminarPaciente(id);
+      return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+
+
+    //buscar paciente con PathVariable
+    @GetMapping("{id}")
+    public ResponseEntity<PacienteSalidaDto> obtenerPacientePorId(@PathVariable Long id){
+        return new ResponseEntity<>(pacienteService.buscarPacientePorId(id), HttpStatus.OK);
+    }
+    @PutMapping("/actualizar")
+    public ResponseEntity<PacienteSalidaDto> actualizarPaciente(@RequestBody PacienteModificacionEntradaDto paciente){
+        return new ResponseEntity<>(pacienteService.actualizarPaciente(paciente),HttpStatus.OK);
+    }
+
+}
